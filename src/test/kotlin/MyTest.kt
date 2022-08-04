ef76cb71367c14c32524eb35a467beeb1b1bc23f
matsuri-gaoka <repetitive789++@gmail.com>
2022-08-04 23:50:47 +0900

    テストケース追加

--- src/test/kotlin/MyTest.kt
+++ src/test/kotlin/MyTest.kt
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test
 internal class MyTest {
     @Test
     fun myTest(){
-        kotlin.test.assertEquals(2,1+1)
+        kotlin.test.assertEquals(2,1+1) //単独要素の比較
+        kotlin.test.assertContentEquals(arrayOf(1, 2, 3), arrayOf(1, 2, 3)) //配列の比較
+        //kotlin.test.assertContentEquals(arrayOf(1, 2, 3), arrayOf(3, 2, 1)) //配列の比較
+        //↑これは失敗する。集合として同じでも、順番も等しくないとダメみたい
     }
 }
